414696fee656a2f4551fbf95a0a898b5830a2a7d
leonid.kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2021-04-17 23:44:47 +0300

    fix photo 1

--- src/main/java/lk/server/cocktails/features/image/ImageController.java
+++ src/main/java/lk/server/cocktails/features/image/ImageController.java
@@ -1,7 +1,9 @@
 package lk.server.cocktails.features.image;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,11 +15,11 @@ import java.io.InputStream;
 public class ImageController {
 
     @ResponseBody
-    @RequestMapping(value = "/photo/{name}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public byte[] photo(HttpServletRequest request, @PathVariable("name") String cocktailName) throws IOException {
+    @GetMapping(value = "/photo/{name}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> photo(HttpServletRequest request, @PathVariable("name") String cocktailName) throws IOException {
         cocktailName = cocktailName.replaceAll("_", " ");
         cocktailName = cocktailName.replaceAll("\\*", "’");
         InputStream in = request.getServletContext().getResourceAsStream("/icons/" + cocktailName + ".jpg");
-        return IOUtils.toByteArray(in);
+        return new ResponseEntity<>(IOUtils.toByteArray(in), HttpStatus.OK);
     }
 }
