bd0fa017e980b17eae4e21f83045250ebb300724
Leonid Kuzmychenko <lleonidk@ukr.net>
2021-07-20 23:21:50 +0300

    add bd configs

--- src/main/java/lk/server/cocktails/CocktailsApplication.java
+++ src/main/java/lk/server/cocktails/CocktailsApplication.java
@@ -2,20 +2,8 @@ package lk.server.cocktails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-        "lk.server.cocktails.features.cocktail.entities",
-        "lk.server.cocktails.features.ingredient.entities",
-        "lk.server.cocktails.features.modes.entities"
-})
-@EnableJpaRepositories(basePackages = {
-        "lk.server.cocktails.features.cocktail.repositories",
-        "lk.server.cocktails.features.ingredient.repositories",
-        "lk.server.cocktails.features.modes.repositories"
-})
 public class CocktailsApplication {
 
     public static void main(String[] args) {
